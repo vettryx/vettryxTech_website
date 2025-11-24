02125316bfce7b0b256807c0bfe0edd6f4f7abcc
André Ventura <ti@asventura.com.br>
2025-11-24 16:57:41 -0300

    feat: atualizar a definição da URL base para suportar variáveis de ambiente

--- utils/api.ts
+++ utils/api.ts
@@ -1,9 +1,7 @@
 // utils/api.ts
 
 // Define a URL base dependendo se está no seu PC ou na Hostinger
-const API_BASE_URL = process.env.NODE_ENV === 'development'
-  ? 'http://localhost:8000' // Docker/Local
-  : 'https://asventura.com.br'; // Produção
+export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000';
 
 export interface Project {
   id: number;
