4f1897b551ff2ff1cf9cf46aae9b4bf21d067b86
André Ventura <ti@asventura.com.br>
2025-11-27 15:17:18 -0300

    feat: habilitar exibição de erros para facilitar o desenvolvimento

--- backend/admin/index.php
+++ backend/admin/index.php
@@ -1,6 +1,10 @@
 <?php
 // backend/admin/index.php
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 session_start();
 require_once __DIR__ . '/../config.php';
 require 'includes/Layout.php';
