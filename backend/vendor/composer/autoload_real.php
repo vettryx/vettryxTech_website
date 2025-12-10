753b200f88f4f1f4a0d902320ab35e791929679f
André Ventura <ti@asventura.com.br>
2025-12-10 15:50:29 -0300

    feat: Add new classes for CSS value parsing
    - Introduced `LineName` class for handling line names in CSS.
    - Added `PrimitiveValue` abstract class as a base for primitive CSS values.
    - Implemented `RuleValueList` class to represent multi-valued CSS rules.
    - Created `Size` class for handling size values with various units.
    - Developed `URL` class to represent URLs in CSS, ensuring proper output format.
    - Established `Value` abstract class as a base for all CSS values, including parsing logic.
    - Introduced `ValueList` abstract class to manage lists of CSS values with separators.

--- backend/vendor/composer/autoload_real.php
+++ backend/vendor/composer/autoload_real.php
@@ -22,8 +22,6 @@ class ComposerAutoloaderInit68062e5bee45c15d0592fde4b404cee4
             return self::$loader;
         }
 
-        require __DIR__ . '/platform_check.php';
-
         spl_autoload_register(array('ComposerAutoloaderInit68062e5bee45c15d0592fde4b404cee4', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInit68062e5bee45c15d0592fde4b404cee4', 'loadClassLoader'));
